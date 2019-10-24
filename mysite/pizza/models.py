216f6b87c0133d97ce210874e8d96088d99e4c9a
meredithcat <meredithcat@gmail.com>
2019-10-24 14:36:23 -0700

    Update models.py

--- mysite/pizza/models.py
+++ mysite/pizza/models.py
@@ -11,6 +11,6 @@ class Pizza(models.Model):
     toppings = models.ManyToManyField(Topping)
 
     def __str__(self):
-        return self.name
+        return "Making a change"
 
 # making some more changes
